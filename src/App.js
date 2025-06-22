c9118f7d2c69d4468703fe33a4078eef8d2efa82
Studio 37 <ceo@ponyboy.win>
2025-06-22 14:38:14 +0000

    Add Cloudflare KV asset handler and update dependencies; remove package.json

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-cat > src/App.js <<EOF
 import React, { useState } from 'react';
 import { Home, Info, Briefcase, Mail, Menu, X, Instagram, Dribbble, Linkedin, Award, Camera, Monitor } from 'lucide-react';
 
@@ -366,4 +340,4 @@ const ContactSection = () => {
 };
 
 export default App;
-EOF
+
